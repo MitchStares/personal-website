0ee292b8eff350c9ffa8c59d859b29eea013565b
MitchStares <mitchell.stares@hotmail.com>
2024-08-11 21:42:18 +1000

    Add clickable card for blogs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar: React.FC = () => {
         </div>
         <div className="space-x-4">
           <Link to="/auth" className="px-4 py-2 border border-green-800 text-green-800 rounded-full hover:bg-green-800 hover:text-white transition">Sign In</Link>
-          <Link to="/map" className="px-4 py-2 bg-green-800 text-white rounded-full hover:bg-green-700 transition">Get started</Link>
+          <Link to="/map" className="px-4 py-2 bg-green-800 text-white rounded-full hover:bg-green-700 transition">Current Project</Link>
         </div>
       </nav>
     </header>
