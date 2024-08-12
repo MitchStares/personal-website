e038473b8e9c7baeeb5b55be3bf87d1655f45840
MitchStares <mitchell.stares@hotmail.com>
2024-08-12 20:49:28 +1000

    Google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebaseConfig';
 import { Layout } from './components/Navbar';
@@ -10,6 +10,10 @@ import MarkdownEditorPage from './pages/MarkdownEditorPage';
 import MapPage from './pages/MapPage';
 import Auth from './components/Auth';
 import ProtectedRoute from './components/ProtectedRoute';
+import { initGA, logPageView } from './analytics';
+
+
+initGA(process.env.REACT_APP_GA_MEASUREMENT_ID || '');
 
 const App: React.FC = () => {
   const [user, setUser] = useState<any>(null);
@@ -25,23 +29,35 @@ const App: React.FC = () => {
   return (
     <Router>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog user={user} />} />
-          <Route path="/blog/:id" element={<BlogDetail user={user} />} />
-          <Route 
-            path="/markdown-editor" 
-            element={
-              <ProtectedRoute user={user}>
-                <MarkdownEditorPage user={user} />
-              </ProtectedRoute>
-            } 
-          />
-          <Route path="/map" element={<MapPage />} />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
+        <AppContent user={user} />
       </Layout>
     </Router>
+  );
+};
+
+const AppContent: React.FC<{ user: any }> = ({ user }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    logPageView(); // Log page view on route change
+  }, [location]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog" element={<Blog user={user} />} />
+      <Route path="/blog/:id" element={<BlogDetail user={user} />} />
+      <Route 
+        path="/markdown-editor" 
+        element={
+          <ProtectedRoute user={user}>
+            <MarkdownEditorPage user={user} />
+          </ProtectedRoute>
+        } 
+      />
+      <Route path="/map" element={<MapPage />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   );
 };
 
---
+++ src/analytics.ts
@@ -0,0 +1,10 @@
+// src/analytics.ts
+import ReactGA from "react-ga4";
+
+export const initGA = (measurementId: string) => {
+  ReactGA.initialize(measurementId);
+};
+
+export const logPageView = () => {
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+};
